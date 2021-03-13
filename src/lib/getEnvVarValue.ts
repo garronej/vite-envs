3c73fe093db91b433c7ef28780225eb854b7690d
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-03-13 22:36:48 +0100

    Change api for getting env

---
+++ src/lib/getEnvVarValue.ts
@@ -0,0 +1,30 @@
+import { nameOfTheGlobal } from "../bin/nameOfTheGlobal";
+
+const errorMessage = [
+    "The environnement variable should have been embedded in the script at build time.",
+    "Check the documentation embed-react-app-envs"
+].join(" ");
+
+export function getEnvVarValue(envVarName: string): string {
+
+    const objectDefinedByThisModule: Record<string, string> = (window as any)[nameOfTheGlobal] ?? {};
+
+    let value: string | undefined = objectDefinedByThisModule[envVarName];
+
+    if (value !== undefined) {
+        return value;
+    }
+
+    if (process.env["NODE_ENV"] === "production") {
+        throw new Error(errorMessage);
+    }
+
+    value = process.env[`REACT_APP_${envVarName}`];
+
+    if (value === undefined) {
+        throw new Error(errorMessage);
+    }
+
+    return value;
+
+}
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
 
-export * from "./getEnv";
+export * from "./getEnvVarValue";
