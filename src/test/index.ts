168e51939285c42146bb2b77e790d6ab2bafeedf
garronej <joseph.garrone.gj@gmail.com>
2022-02-13 18:50:29 +0100

    Update .gitatributes so no vanila JS appears in language statistic

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,14 +1,16 @@
-import { downloadAndUnzip } from "../tools/downloadAndUnzip";
+//import { downloadAndUnzip } from "../tools/downloadAndUnzip";
 import * as st from "scripting-tools";
 import { join as pathJoin } from "path";
 import { getProjectRoot } from "../tools/getProjectRoot";
 
 const sampleProjectDirPath = pathJoin(getProjectRoot(), "sample_project");
 
+/*
 downloadAndUnzip({
     "url": "https://github.com/garronej/cra-envs/releases/download/ASSETS/sample_project_4.zip",
     "destDirPath": sampleProjectDirPath
 });
+*/
 
 const bin = require(pathJoin(getProjectRoot(), "package.json"))["bin"];
 
