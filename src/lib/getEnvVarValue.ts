a919aa190bf745931ee7d98541750b7da56e34cf
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-04 03:59:22 +0100

    Don't crash if no injection in production

--- src/lib/getEnvVarValue.ts
+++ src/lib/getEnvVarValue.ts
@@ -1,12 +1,5 @@
 import { nameOfTheGlobal } from "../bin/nameOfTheGlobal";
 
-const getErrorMessage = (envVarName: string)=>[
-    "This project is using cra-envs however it seems that",
-    "the script 'npx embed-environnement-variables' hasn't been",
-    `run. As a result the value of ${envVarName} was not bundled`,
-    "in the script. Documentation: https://github.com/garronej/cra-envs"
-].join(" ");
-
 export function getEnvVarValue(envVarName: string): string {
 
     const objectDefinedByThisModule: Record<string, string> = (window as any)[nameOfTheGlobal] ?? {};
@@ -15,10 +8,6 @@ export function getEnvVarValue(envVarName: string): string {
 
     if (value !== undefined) {
         return value;
-    }
-
-    if (process.env["NODE_ENV"] === "production") {
-        throw new Error(getErrorMessage(envVarName));
     }
 
     value = process.env[`REACT_APP_${envVarName}`];
