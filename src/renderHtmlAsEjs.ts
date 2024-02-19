d6b54af65987b4b6351ab1a8ee6cac35ccc1aeee
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-02-19 06:14:14 +0100

    working

--- src/renderHtmlAsEjs.ts
+++ src/renderHtmlAsEjs.ts
@@ -1,27 +1,42 @@
 import ejs from "ejs";
 import { multiReplace } from "./tools/multiReplace";
 import JSON5 from "json5";
+import { replaceAll } from "./tools/String.prototype.replaceAll";
+import * as cheerio from "cheerio";
 
 export function renderHtmlAsEjs(params: { html: string; env: Record<string, string> }): string {
     const { html, env } = params;
 
-    let renderedHtml = ejs.render(
-        html,
+    const placeholder = "__xLd9dwJsZz32s__";
+
+    let renderedHtml = replaceAll(html, "import.meta.env", placeholder);
+
+    renderedHtml = ejs.render(
+        renderedHtml,
         {
-            "import": {
-                "meta": {
-                    env
-                }
-            },
+            [placeholder]: env,
             JSON5
         },
         { "escape": str => str }
     );
 
+    renderedHtml = replaceAll(renderedHtml, placeholder, "import.meta.env");
+
     renderedHtml = multiReplace({
         "input": renderedHtml,
         "keyValues": Object.fromEntries(Object.entries(env).map(([key, value]) => [`%${key}%`, value]))
     });
+
+    const $ = cheerio.load(renderedHtml);
+
+    $("*")
+        .contents()
+        .filter(function () {
+            return this.type === "comment";
+        })
+        .remove();
+
+    renderedHtml = $.html();
 
     return renderedHtml;
 }
