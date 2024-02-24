4e85304ca5e3378ef124937ee8fe726d23bc85dc
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-02-24 03:32:14 +0100

    Prevent all kind of corruption of the html that might be caused by injecting the envs

--- src/injectScriptToDefineGlobal.ts
+++ src/injectScriptToDefineGlobal.ts
@@ -1,5 +1,6 @@
 import type * as cheerio from "cheerio";
 import { nameOfTheGlobal } from "./constants";
+import { symToStr } from "tsafe/symToStr";
 
 export function injectScriptToDefineGlobal(params: {
     $: cheerio.CheerioAPI;
@@ -12,9 +13,12 @@ export function injectScriptToDefineGlobal(params: {
 
     $(`head script[${scriptPropertyKey}="${scriptPropertyValue}"]`).remove();
 
+    const base64EncodedStringifiedEnv = Buffer.from(JSON.stringify(env), "utf8").toString("base64");
+
     const newScript = [
         `<script ${scriptPropertyKey}="${scriptPropertyValue}">`,
-        `   window.${nameOfTheGlobal}= ${JSON.stringify(env)};`,
+        `   const ${symToStr({ base64EncodedStringifiedEnv })} = "${base64EncodedStringifiedEnv}";`,
+        `   window.${nameOfTheGlobal}= JSON.parse(atob(${symToStr({ base64EncodedStringifiedEnv })}));`,
         `</script>`
     ].join("\n");
 
